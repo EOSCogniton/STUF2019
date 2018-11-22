6642c4fa490dbdd8147f1e945268d7d7bb2b538c
Unknown <arthur.rodriguez@aliceadsl.fr>
2018-11-22 20:09:28 +0100

    Thermocouple

---
+++ EL - Electrical/Autre/Thermocouple/Thermocouple/Thermocouple.ino
@@ -0,0 +1,16 @@
+#include "High_Temp.h"
+
+HighTemp ht(A1, A0);
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("grove - hight temperature sensor test demo");
+    ht.begin();
+}
+
+void loop()
+{
+    Serial.println(ht.getThmc());
+    delay(100);
+}
