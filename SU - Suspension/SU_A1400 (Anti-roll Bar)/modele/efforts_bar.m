be410403adc40836acb089899e69f9ee1580f386
Erwan <edelongcamp@gmail.com>
2018-11-22 18:16:40 +0100

    MAJ des vieux fichiers

--- SU - Suspension/SU_A1400 (Anti-roll Bar)/modele/efforts_bar.m
+++ SU - Suspension/SU_A1400 (Anti-roll Bar)/modele/efforts_bar.m
@@ -10,3 +10,16 @@ Ks=2; % coefficient de sécurité
 b=6*Ks*F*A/(h^2*Re)
 
 masse=7800*A*b*h
+
+% Calcul du motion ratio de la barre
+% MR_arb = angle de roulis/torsion de la barre
+L=0.488/2;
+F=1050;
+A=0.124;
+G=81000*10^6;
+D=0.017;
+d=0.0125;
+phi=(32*L*F*A)/(pi*G*(D^4-d^4))
+MR_arb=0.8*1.600/(phi*0.05)
+% MR = 5 à l'avant
+% MR = 12 à l'arrière
